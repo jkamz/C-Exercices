61641795b2d0e34800c5960d0c42950fa4395db8
jkamz <jkamz6432@gmail.com>
2019-10-10 03:28:18 +0300

    array of strings

---
+++ ex7.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	int i = 0;
+	
+	//Print each string in argv
+	for(i = 1; i < argc; i++){
+		printf("arg %d: %s\n", i, argv[i]);
+	}
+	
+	//custom array of strings
+	char *cities[] = {
+		"Nairobi", "Mombasa", "Nakuru", "Kisumu"
+	};
+	
+	int num_cities = 6;
+	
+	for(i = 0; i < num_cities; i++) {
+		printf("city %d: %s\n", i, cities[i]);
+	}
+	
+	return 0;
+}
